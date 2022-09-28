0545c3edf152533df14e97afc2c597103eb01e11
prasbeck <ch20b083@smail.iitm.ac.in>
2022-09-29 03:21:14 +0530

    Dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [],
   theme: {
     debugScreens: {
       position: ['top', 'left'],
       },
-    extend: {},
+    extend: {
+      fontFamily: {
+      headline: ['Oswald']
+      },
+      colors: {
+        mainColor: '#212f49'
+      }
+    }
   },
   plugins: [
     require('tailwindcss-debug-screens'),
